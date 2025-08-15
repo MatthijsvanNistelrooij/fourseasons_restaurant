da2328bdd2f0d718ae4ea810d92b1355f3c3fcfe
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-16 00:42:03 +0200

    update Hero

--- src/app/components/MenuSection.tsx
+++ src/app/components/MenuSection.tsx
@@ -6,7 +6,7 @@ export default function MenuSection() {
   return (
     <section className="bg-[#1a1a1a] text-white">
       <div
-        className="max-w-3xl mx-auto px-4 py-24 flex justify-between bg-no-repeat bg-cover bg-[position:center_100%]"
+        className="max-w-3xl mx-auto px-4 py-24 flex flex-col md:flex-row justify-between bg-no-repeat bg-cover bg-[position:center_100%] gap-8"
         style={{
           fontFamily: "var(--font-body)",
           backgroundImage: "url('/assets/four_seasons_leaf.png')",
@@ -19,7 +19,7 @@ export default function MenuSection() {
           MENU
           <ReservationButton />
         </div>
-        <div className="bg-[#1a1a1a] border-[#cda36b] border-r border-b p-10 flex flex-col items-center gap-3">
+        <div className="bg-[#1a1a1a] border-[#cda36b] border p-10 py-20 flex flex-col items-center gap-3">
           <Image src={logo} alt="logo" height={200} width={200} />
           <div
             className="mt-20 border-t-[0.5px] border-b-[0.5px] border-[#f0f0f0] px-6 py-3 inline-block font-light"
