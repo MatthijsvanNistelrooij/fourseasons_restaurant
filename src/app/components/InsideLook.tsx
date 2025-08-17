969bd50f8e006cec2fb2089df05410712e6ed6ee
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-17 04:12:38 +0200

    update inside

--- src/app/components/InsideLook.tsx
+++ src/app/components/InsideLook.tsx
@@ -3,20 +3,20 @@ import ImageCarousel from "./ImageCarousel"
 
 const InsideLook = () => {
   return (
-    <div id="inside" className="mt-10 x-4 py-16 max-w-7xl mx-auto text-center">
-      <div className="mx-auto text-center max-w-5xl">
+    <div id="inside" className="mt-10 x-4 py-16 max-w-4xl mx-auto text-center">
+      <div className="mx-auto text-center">
         <h1
           className="text-2xl -tracking-tight mb-4 mt-10"
           style={{
             fontFamily: "Mayfair Display, serif",
-            transform: "scaleY(1.3)", // 1.0 = normaal, >1 = hoger
-            display: "inline-block", // nodig zodat de scale alleen dit element pakt
+            transform: "scaleY(1.3)", 
+            display: "inline-block", 
           }}
         >
           INSIDE LOOK
         </h1>
         <p
-          className="text-2xl font-light md:text-5xl text-[#f0f0f0] leading-15 mb-12"
+          className="text-2xl font-light md:text-4xl text-[#f0f0f0] leading-15 mb-12"
           style={{ fontFamily: "Lato, sans-serif" }}
         >
           Onze lunch- en dinercreaties worden met liefde bereid, geïnspireerd
