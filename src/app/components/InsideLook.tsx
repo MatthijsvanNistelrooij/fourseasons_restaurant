4247dace643b577d9722cb62c295090189717cf6
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-17 05:49:16 +0200

    update menu

--- src/app/components/InsideLook.tsx
+++ src/app/components/InsideLook.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const InsideLook = () => {
   return (
-    <div id="inside" className="mt-10 x-4 py-16 max-w-4xl mx-auto text-center">
+    <div id="inside" className="mt-10 x-4 py-16 max-w-5xl mx-auto text-center">
       <div className="mx-auto text-center">
         <h1
           className="text-2xl -tracking-tight mb-4 mt-10"
@@ -15,7 +15,7 @@ const InsideLook = () => {
           INSIDE LOOK
         </h1>
         <p
-          className="text-2xl font-light md:text-4xl text-[#f0f0f0] leading-15 mb-12 p-5"
+          className="text-2xl font-light md:text-5xl text-gray-300 leading-14 mb-12 p-5"
           style={{ fontFamily: "Lato, sans-serif" }}
         >
           Onze lunch- en dinercreaties worden met liefde bereid, geïnspireerd
