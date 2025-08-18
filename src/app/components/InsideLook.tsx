47ddcd3bbad1211ccf9cd694e8eeea483e0d366f
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-19 00:18:09 +0200

    added auth setup

--- src/app/components/InsideLook.tsx
+++ src/app/components/InsideLook.tsx
@@ -15,7 +15,7 @@ const InsideLook = () => {
           INSIDE LOOK
         </h1>
         <p
-          className="text-2xl font-light md:text-4xl lg:text-6xl text-gray-300 leading-12 md:leading-17 px-4 py-6"
+          className="text-2xl font-light md:text-4xl lg:text-6xl text-gray-300 leading-12 md:leading-19 px-4 py-6"
           style={{ fontFamily: "Lato, sans-serif" }}
         >
           Onze lunch- en dinercreaties worden met liefde bereid, geïnspireerd
