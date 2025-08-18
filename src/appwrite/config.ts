05dfda4c18cc14a8ef64fd70d59a632bf5eaa28a
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-19 00:05:50 +0200

    added auth setup

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -2,6 +2,5 @@ export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
-  appointmentsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_APPOINTMENTS!,
-  apiKey: process.env.APPWRITE_API_KEY!,
+  reservationsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_RESERVATIONS_COLLECTION!,
 }
