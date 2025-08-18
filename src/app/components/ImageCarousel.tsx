ffe3683d5b3c598720039bb25aeefa1386a18d29
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-18 05:45:11 +0200

    updated hero

--- src/app/components/ImageCarousel.tsx
+++ src/app/components/ImageCarousel.tsx
@@ -4,7 +4,7 @@ import img1 from "/public/assets/img_1.jpg"
 import img2 from "/public/assets/img_2.jpg"
 import img3 from "/public/assets/img_3.jpg"
 import img4 from "/public/assets/img_4.jpg"
-import img5 from "/public/assets/img_1.jpg"
+import img5 from "/public/assets/img_5.jpg"
 
 const ImageCarousel = () => {
   const images = [img1, img2, img3, img4, img5]
