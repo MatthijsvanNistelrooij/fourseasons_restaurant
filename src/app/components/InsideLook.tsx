81eaf84885faa1802630e24bf888cd1de20e927d
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-18 22:06:29 +0200

    update font

--- src/app/components/InsideLook.tsx
+++ src/app/components/InsideLook.tsx
@@ -5,7 +5,7 @@ const InsideLook = () => {
     <div id="inside" className="px-4 pt-10 md:py-24 max-w-7xl mx-auto text-center">
       <div className="mx-auto text-center">
         <h1
-          className="md:text-3xl -tracking-tight mt-10"
+          className="md:text-2xl lg:text-3xl -tracking-tight mt-10"
           style={{
             fontFamily: "Mayfair Display, serif",
             transform: "scaleY(1.3)",
@@ -15,7 +15,7 @@ const InsideLook = () => {
           INSIDE LOOK
         </h1>
         <p
-          className="text-2xl font-light md:text-6xl text-gray-300 leading-12 md:leading-17 px-4 py-6"
+          className="text-2xl font-light md:text-4xl lg:text-6xl text-gray-300 leading-12 md:leading-17 px-4 py-6"
           style={{ fontFamily: "Lato, sans-serif" }}
         >
           Onze lunch- en dinercreaties worden met liefde bereid, geïnspireerd
