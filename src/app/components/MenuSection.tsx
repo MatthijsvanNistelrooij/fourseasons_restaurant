6bf279a1052e8c50078ca6f5edd5414aacbb6c64
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-18 06:05:19 +0200

    updated hero

--- src/app/components/MenuSection.tsx
+++ src/app/components/MenuSection.tsx
@@ -7,7 +7,7 @@ export default function MenuSection() {
   return (
     <section
       id="menu"
-      className="relative text-[#f0f0f0] mb-20 scroll-mt-20 md:mb-30 flex items-center justify-center overflow-hidden"
+      className="relative text-[#f0f0f0] mb-20 md:mb-30 flex items-center justify-center overflow-hidden"
     >
       <div className="absolute inset-0 -z-20 overflow-hidden">
         <Image
