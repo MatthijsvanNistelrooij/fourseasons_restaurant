c500f9bcb64300272b3d457833a2e930ab87bf9f
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-08-19 00:09:02 +0200

    added auth setup

--- src/appwrite/index.ts
+++ src/appwrite/index.ts
@@ -1,6 +1,12 @@
 import { Client, Account, Databases } from "appwrite"
 import { appwriteConfig } from "../appwrite/config"
 
+if (!appwriteConfig.endpointUrl || !appwriteConfig.projectId) {
+  throw new Error(
+    "Appwrite config is missing. Check your environment variables."
+  )
+}
+
 const client = new Client()
   .setEndpoint(appwriteConfig.endpointUrl)
   .setProject(appwriteConfig.projectId)
@@ -9,4 +15,3 @@ const account = new Account(client)
 const databases = new Databases(client)
 
 export { client, databases, account }
-
